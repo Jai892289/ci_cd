22a4904caa82adc62b8fac2fa962a9609ac84200
Jai892289 <gjai4341@gmail.com>
2026-05-06 10:07:55 +0530

    updated text

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "working 🚀" });
+  res.json({ message: "working code 🚀" });
 });
 
 export default app;
