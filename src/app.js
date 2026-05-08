412a19dbf70a5b4de2606f497d892de93cfa85ec
Jai892289 <gjai4341@gmail.com>
2026-05-08 15:40:19 +0530

    all

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "working code 🚀" });
+  res.json({ message: "working code we 🚀" });
 });
 
 export default app;
